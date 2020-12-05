4bdd7595bd1e29f65a81cd08ee9810df20fd79cb
2ujin <suddenlydisappear200@gmail.com>
2020-12-05 23:15:08 +0900

    필요한 component 및 route 모두 생성!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardViewComponent } from './main/card-view/card-view.component';
+import { CardComponent } from './main/card/card.component';
+import { HomeComponent } from './main/home/home.component';
+import { MainComponent } from './main/main.component';
+import { MyInfoComponent } from './main/my-info/my-info.component';
+import { SendCardComponent } from './main/send-card/send-card.component';
+import { WriteCardComponent } from './main/write-card/write-card.component';
+import { MailBoxComponent } from './public/mail-box/mail-box.component';
+import { PublicComponent } from './public/public.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'public', // 웹에서 보여지는 화면이 아닌 컴포넌트들
+    component: PublicComponent,
+    children: [
+      {
+        path: 'mail-box', // 공유한 카드 보는 컴포넌트라 분리가 필요.
+        component: MailBoxComponent
+      },
+    ]
+  },
+  {
+    path: 'main', // 웹에서 보여지는 컴포넌트 (상단, 햄버거바 기본)
+    component: MainComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'card', component: CardComponent },
+      { path: 'card-view', component: CardViewComponent },
+      { path: 'my-info', component: MyInfoComponent },
+      { path: 'send-card', component: SendCardComponent }, // 보낸카드 모아보는 컴포넌트
+      { path: 'write-card', component: WriteCardComponent },
+    ]
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main/card-view/card-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-view',
+  templateUrl: './card-view.component.html',
+  styleUrls: ['./card-view.component.scss']
+})
+export class CardViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
